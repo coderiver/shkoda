e166983e905e36a3de2d5afc87bce7b05265cdaa
Georgiy <goshazvir@gmail.com>
2014-05-05 17:11:51 +0300

    init

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 head.ready(document, function () {
-	console.log($('body').html());
+	// console.log($('body').html());
+
+	$(function() {
+	    var footerHeight = $(".wrap-footer").height();
+	    $(".out").css("margin-bottom", -footerHeight);
+	    $(".push").css("height", footerHeight);
+	});
+
+	$(window).resize(function() {
+	    var footerHeight = $(".wrap-footer").height();
+	    $(".out").css("margin-bottom", -footerHeight);
+	    $(".push").css("height", footerHeight);
+	});
 });
